7c6d6d54fd2ae7555b91fc54e6987b021f41756f
Diogo <dguimaraes@live.com.pt>
2025-12-09 23:20:35 +0000

    [UPDATE] - Batchfile fo windows

--- tools/unbrick_k5_v1-windows/unbrick_k5_v1.bat
+++ tools/unbrick_k5_v1-windows/unbrick_k5_v1.bat
@@ -1,4 +1,4 @@
 @echo off
-REM --- Script para ejecutar OpenOCD en Windows ---
+REM --- Script for Windows OpenOCD.exe ---
 openocd.exe -f ./interface/stlink.cfg -f ./target/dp32g030.cfg -c "init; reset halt; uv_flash_bl bootloader.bin; shutdown"
 pause
