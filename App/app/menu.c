c8e750e24e44db34a0c192af30cf8c1271fcc414
ozitoune <116393936+ozitoune@users.noreply.github.com>
2025-12-16 15:33:04 +0100

    Fix indentation for MENU_SAVE

--- App/app/menu.c
+++ App/app/menu.c
@@ -1109,7 +1109,7 @@ void MENU_ShowCurrentSetting(void)
 
         
         
-            case MENU_SAVE:
+        case MENU_SAVE:
             gSubMenuSelection = gEeprom.BATTERY_SAVE;
             break;
 
