64dbb4c0d2da30392c06abf84099f28a42a18097
Kristijan Gjoshev <crypter@mail.com>
2026-08-17 14:18:06 +0200

    Fix indentation and formatting in flashlight.c

--- App/app/flashlight.c
+++ App/app/flashlight.c
@@ -65,11 +65,10 @@ static inline void Flashlight_Toggle(){ GPIO_TogglePin(GPIO_PIN_FLASHLIGHT); }
 
         if (gFlashLightButtonTimeout_10ms == 0 && gFlashLightState != FLASHLIGHT_OFF) {
             Flashlight_TurnOff();
-	    gFlashLightState = FLASHLIGHT_OFF;
-	    return;
+	        gFlashLightState = FLASHLIGHT_OFF;
+	        return;
         }
         gFlashLightButtonTimeout_10ms = flashlight_button_countdown_10ms;
-
 
         if(gFlashLightState == FLASHLIGHT_OFF) {
             Flashlight_TurnOn();
